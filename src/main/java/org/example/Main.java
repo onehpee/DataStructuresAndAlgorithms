a71958f4498ed4ac153ffbc7d119b8bbf1a5865e
Adedayo UWensuyi <onehpee@yahoo.com>
2023-11-09 18:24:39 -0600

    Stack

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import org.example.model.ShoppingItem;
+import org.example.stack.Stack;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack cart = new Stack(5);
+
+        cart.push(new ShoppingItem("pizza", 14.99));
+        cart.push(new ShoppingItem("hotdog", 4.99));
+        cart.push(new ShoppingItem("pancake", 9.99));
+        cart.push(new ShoppingItem("coffee", 2.99));
+        cart.push(new ShoppingItem("egusi", 78.99));
+        cart.push(new ShoppingItem("waffle", 7.99));
+
+     //  cart.pop();
+
+
+       //cart.printStack();
+        cart.peek();
+
+
+
 
     }
 }
--- src/main/java/org/example/stack/Stack.java
+++ src/main/java/org/example/stack/Stack.java
@@ -20,4 +20,34 @@ public class Stack {
     public boolean isFull(){
         return size >= capacity;
     }
+
+    public boolean push(ShoppingItem item){
+        if(isFull()){
+            return false;
+        }
+        cart[size++] = item;
+        return true;
+    }
+
+    public boolean pop(){
+        if(isEmpty()){
+            return false;
+        }
+        cart[--size] = null;
+        return true;
+    }
+
+    public void peek(){
+        if(isEmpty()){return;}
+        ShoppingItem item = cart[size-1];
+        System.out.println(cart[size-1].name);
+    }
+
+    public void printStack(){
+        for (int i = size-1; i >= 0; i--){
+            System.out.println(cart[i]);
+        }
+    }
 }
+
+
